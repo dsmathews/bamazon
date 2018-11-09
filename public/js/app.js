37546924630cd740161b9517ca603fe4684070c5
Darrell Mathews <smathewsbd@gmail.com>
2018-11-09 16:24:30 -0500

    adds config info

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,17 @@ $(function () {
     let productList = [];
     let cart = 0;
 
-    const render = function(items) {
+    const render = function (items) {
         $('modal').modal('hide');
         $('#sale-items').empty();
 
-        items.forEach(function(item) {
+        items.forEach(function (item) {
             $('#sale-items').append(buildRow(item));
-        });        
+        });
     };
 
     const getItems = function () {
-        $.get('/api/Products').done(render);
+        $.get('/api/Products').then(render);
         // console.log(getItems);
     };
 
@@ -22,25 +22,35 @@ $(function () {
         const input = $('<input>').attr({
             type: 'number',
             min: 0,
-            id: item.id
+            'data-id': item.id
         });
+
         const button = $('<button>')
-        .addClass('btn-warning-add-to-cart')
-        .text('Add to Cart')
-        .attr('data-id', item.id);
+            .addClass('btn-warning-add-to-cart addToCart')
+            .text('Add to Cart')
+            .attr('data-id', item.id);
 
         //appending each item to the div
         tr.append(
-            $('<td>').append(input),
-            $('<td>').text(item.product_name),
-            $('<td>').text(item.stock_quantity),
-            $('<td>').text(`$${item.price}`),
+            $('<td class="input">').append(input),
+            $('<td class= "productName" >').text(item.product_name),
+            $('<td class="stock">').text(item.stock_quantity),
+            $('<td class= "price">').text(`$${item.price}`),
             $('<td>').append(button)
         );
         return tr;
     };
-    
 
-    getItems();
+    const numberItems = function () {
+        let uniqueID = $(this).attr("data-id");
+        console.log(uniqueID);
+        console.log($('input').attr(`data-id=${uniqueID}`).val())  
+
+
+    
+    };
+
+$('#sale-items').on('click', '.addToCart', numberItems);
+getItems();
     
 });
